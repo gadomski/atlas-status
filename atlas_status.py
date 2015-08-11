ab2ad480dea15f128ba1faec96ae72cda58d324a
Pete Gadomski <pete.gadomski@gmail.com>
2015-08-11 13:24:18 -0400

    Fix to get the latest image

--- atlas_status.py
+++ atlas_status.py
@@ -14,11 +14,11 @@ app = Flask(__name__)
 def atlas_status():
     values = atlas.get_messages("/var/iridium/300234063909200")[-1]
     last_image_filename = os.path.basename(
-            glob.glob("/home/iridiumcam/StarDot/ATLAS_CAM/*.jpg")[-1])
+            sorted(glob.glob("/home/iridiumcam/StarDot/ATLAS_CAM/*.jpg"))[-1])
     values["last_image_src"] = "http://iridiumcam.lidar.io/ATLAS_CAM/" + \
             last_image_filename
     return render_template("atlas_status.html", **values)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
