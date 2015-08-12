129b0a336f847f548b5115763824193dcbc4ba6f
Pete Gadomski <pete.gadomski@gmail.com>
2015-08-12 11:42:29 -0600

    Setup local configuration

--- atlas_status.py
+++ atlas_status.py
@@ -7,17 +7,16 @@ from flask import Flask, render_template, make_response
 
 import atlas
 
-MESSAGE_DIRECTORY = "/var/iridium/300234063909200"
-
-
 app = Flask(__name__)
+app.config["MESSAGE_DIRECTORY"] = "/var/iridium/300234063909200"
+app.config["IMAGE_DIRECTORY"] = "/home/iridiumcam/StarDot/ATLAS_CAM"
 
 
 @app.route("/")
 def atlas_status():
-    values = atlas.get_messages(MESSAGE_DIRECTORY)[-1]
-    last_image_filename = os.path.basename(
-            sorted(glob.glob("/home/iridiumcam/StarDot/ATLAS_CAM/*.jpg"))[-1])
+    values = atlas.get_messages(app.config["MESSAGE_DIRECTORY"])[-1]
+    image_glob = os.path.join(app.config["IMAGE_DIRECTORY"], "*.jpg")
+    last_image_filename = os.path.basename(sorted(glob.glob(image_glob))[-1])
     values["last_image_src"] = "http://iridiumcam.lidar.io/ATLAS_CAM/" + \
             last_image_filename
     values["last_image_datetime"] = datetime.datetime.strptime(
@@ -29,7 +28,7 @@ def atlas_status():
 @app.route("/data.csv")
 def atlas_data():
     output = StringIO.StringIO()
-    atlas.write_data(MESSAGE_DIRECTORY, output)
+    atlas.write_data(app.config["MESSAGE_DIRECTORY"], output)
     response = make_response(output.getvalue())
     response.headers["Content-Disposition"] = "attachment; filename=data.csv"
     response.headers["Content-type"] = "text/csv"
@@ -37,4 +36,5 @@ def atlas_data():
 
 
 if __name__ == "__main__":
+    app.config["IMAGE_DIRECTORY"] = "/Users/gadomski/Pictures/ATLAS_CAM"
     app.run(debug=True, host="0.0.0.0")
