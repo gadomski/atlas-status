3c508ab67e794aa106d6f8fee33d3cf182e0e2e5
Pete Gadomski <pete.gadomski@gmail.com>
2015-08-18 17:56:10 -0600

    Add latest.json

--- atlas_status.py
+++ atlas_status.py
@@ -1,5 +1,6 @@
 import datetime
 import glob
+import json
 import os
 import StringIO
 
@@ -12,8 +13,13 @@ app.config["MESSAGE_DIRECTORY"] = "/var/iridium/300234063909200"
 app.config["IMAGE_DIRECTORY"] = "/home/iridiumcam/StarDot/ATLAS_CAM"
 
 
-@app.route("/")
-def atlas_status():
+def json_serializer(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    raise TypeError("Type is not serializable")
+
+
+def get_latest_message():
     messages = atlas.get_messages(app.config["MESSAGE_DIRECTORY"])
     values = messages[-1]
 
@@ -24,8 +30,13 @@ def atlas_status():
     values["last_image_datetime"] = datetime.datetime.strptime(
         os.path.splitext(last_image_filename)[0],
         "ATLAS_CAM_%Y%m%d_%H%M%S")
+    return values
 
-    return render_template("atlas_status.html", **values)
+
+@app.route("/")
+def atlas_status():
+    message = get_latest_message()
+    return render_template("atlas_status.html", **message)
 
 
 @app.route("/data.csv")
@@ -35,6 +46,15 @@ def atlas_data():
     response = make_response(output.getvalue())
     response.headers["Content-Disposition"] = "attachment; filename=data.csv"
     response.headers["Content-type"] = "text/csv"
+    return response
+
+
+@app.route("/latest.json")
+def atlas_latest():
+    message = get_latest_message()
+    response = make_response(json.dumps(message, default=json_serializer))
+    response.headers["Content-Disposition"] = "attachment; filename=latest.json"
+    response.headers["Content-type"] = "application/json"
     return response
 
 
